fa67224e81b72f977e6bcaf48b145a44e4643ae7
emarfer213 <emiliomartinf2002@gmail.com>
2025-11-27 19:30:20 +0100

    practica pre examen

--- lib/tema 2/Tarea de clase 1/main.dart
+++ lib/tema 2/Tarea de clase 1/main.dart
@@ -24,26 +24,60 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Expanded(
-        child: campo(nombre: '',),
+        child: Campo(),
       )
     );
   }
 }
 
-class campo extends StatelessWidget {
-  const campo({super.key, required this.nombre});
-  final String nombre;
-  
+class Campo extends StatefulWidget {
+  const Campo({super.key});
+
+  @override
+  State<Campo> createState() => _CampoState();
+}
+
+class _CampoState extends State<Campo> {
+  int? _selectedIndex;
+  bool _isRightToLeft = false;
+  double _initialDx = 0.0;
+  double _lastDx = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: ListView.builder(
         itemCount: 5,
-          itemBuilder: (context, index){
+        itemBuilder: (context, index) {
           return GestureDetector(
+            onHorizontalDragStart: (details) {
+              _initialDx = details.localPosition.dx;
+              _lastDx = _initialDx;
+            },
+            onHorizontalDragUpdate: (details) {
+              _lastDx = details.localPosition.dx;
+              _isRightToLeft = details.delta.dx < 0;
+            },
+            onHorizontalDragEnd: (details) {
+              double distance = (_initialDx - _lastDx).abs();
 
+              if (_isRightToLeft && distance > 100) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              }
+            },
+            child: ListTile(
+              tileColor: index == _selectedIndex
+                  ? Colors.grey[200]
+                  : Colors.transparent,
+              title: Text(
+                '$index',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
           );
-          }
+        },
       ),
     );
   }
